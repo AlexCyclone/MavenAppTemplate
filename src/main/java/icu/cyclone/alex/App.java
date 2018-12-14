8507ac6444b89e303731bd734fbc2ff4e235c6c2
AlexCyclone <aleksey.babanin@gmail.com>
2018-12-14 22:35:35 +0200

    ok

---
+++ src/main/java/icu/cyclone/alex/App.java
@@ -0,0 +1,15 @@
+package icu.cyclone.alex;
+
+import java.util.Arrays;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" + (args.length > 0 ? " args" + Arrays.toString(args) :""));
+    }
+}
